3828046035fdce1db167388bb4b941ca3fa9c894
Marin-Peptenaru <marin.peptenaru@protonmail.com>
2021-03-22 23:34:53 +0200

    Created Expense class with essential members for the moment. Added some dummy data into the homepage

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,11 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String _title;
-  final List<Expense> expenses = [];
+  final List<Expense> expenses = [
+    Expense(id: 1, title: 'New game', amount: 20.0, date: DateTime.now()),
+    Expense(id: 2, title: 'Milk', amount: 6.5, date: DateTime.now()),
+    Expense(id: 3, title: 'Pizza', amount: 15.0, date: DateTime.now()),
+  ];
 
   MyHomePage({@required String title}) : _title = title;
 
@@ -38,7 +42,13 @@ class MyHomePage extends StatelessWidget {
                 width: double.infinity,
                 child: Card(
                     color: Colors.blue, child: Text('Chart'), elevation: 5)),
-            Card(color: Colors.red, child: Text('Expenses'))
+            Column(
+              children: [
+                ...expenses
+                    .map((expense) => Card(child: Text(expense.title)))
+                    .toList(),
+              ],
+            )
           ],
         ));
   }
